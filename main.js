cc69dee7b759a844409c827f0e288fb17d1da9f1
7Demon <dickyramadhan543@gmail.com>
2025-03-08 22:05:26 +0700

    belajar js hari pertama

---
+++ main.js
@@ -0,0 +1,13 @@
+// alert("helo world");
+// console.log('hello world');
+// var test = prompt("siapa nama kamu");
+// alert('nama kamu adalah '+test);
+
+let usia = Number(prompt('berapa usia mu'));
+if (usia >= 1 && usia <= 15){
+    usia = 'masih kecil';
+}else{
+    usia = 'dewasa';
+}
+
+alert('umur kamu adalah '+ usia);
